197bdb4206549cc8d77e83a3c216dc3c5a6b4676
jolson615 <jolson615@gmail.com>
2019-08-13 20:49:17 -0400

    erases solution

--- src/Components/Secret.js
+++ src/Components/Secret.js
@@ -13,13 +13,9 @@ const Secret = (props) => {
   component.render = () => {
     return (
       <div className="Secret" >
-        <h1>Secret word {props.title}</h1>
-        <h2>{component.state}</h2>
+        <h1>Secret word INPUT_TITLE_HERE</h1>
         <div className="word-block">
-          <h3 className={component.checkActive(props.words[0])} onClick={component.handleClick}>{props.words[0]}</h3>
-          <h3 className={component.checkActive(props.words[1])} onClick={component.handleClick}>{props.words[1]}</h3>
-          <h3 className={component.checkActive(props.words[2])} onClick={component.handleClick}>{props.words[2]}</h3>
-          <h3 className={component.checkActive(props.words[3])} onClick={component.handleClick}>{props.words[3]}</h3>
+          <h3 className="word" onClick={component.handleClick}>INPUT_WORD1_HERE</h3>
         </div>
       </div>
     )
